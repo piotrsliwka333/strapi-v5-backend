b1137755edfa0cbd3d98e97e6ad8ac0db01d6310
Peter Sliwka <piotrsliwka333@gmail.com>
2024-10-16 17:36:03 +0200

    Main - remove unused code

--- config/database.ts
+++ config/database.ts
@@ -51,21 +51,3 @@ export default ({ env }) => {
     },
   };
 };
-
-// export default ({ env }) => ({
-//   connection: {
-//     client: 'postgres',
-//     connection: {
-//       host: env('DATABASE_HOST'),
-//       port: env.int('DATABASE_PORT'),
-//       database: env('DATABASE_NAME'),
-//       user: env('DATABASE_USERNAME'),
-//       password: env('DATABASE_PASSWORD'),
-//       schema: env('DATABASE_SCHEMA'), // Not required
-//       ssl: {
-//         rejectUnauthorized: env.bool('DATABASE_SSL_SELF'),
-//       },
-//     },
-//     // debug: false,
-//   },
-// });
